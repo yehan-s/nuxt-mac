d2e0dfecbcc745504637a8c1ef3c696ba94ffea8
yehan <1513849675@qq.com>
2024-03-27 10:58:17 +0800

    feat:static image

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@pinia/nuxt", 'dayjs-nuxt', "@nuxtjs/tailwindcss"],
+  modules: [
+    [
+      "@pinia/nuxt",
+      {
+        autoImports: [
+          // 自动引入 `defineStore()`
+          "defineStore",
+          // 自动引入 `defineStore()` 并重命名为 `definePiniaStore()`
+          ["defineStore", "definePiniaStore"],
+        ],
+      },
+    ],
+    "dayjs-nuxt",
+    "@nuxtjs/tailwindcss",
+  ],
+  devServer: {
+    port: 3001,
+  },
 });
